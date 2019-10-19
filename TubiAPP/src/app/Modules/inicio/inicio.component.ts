3e8ffc5bae663cc62cf5d53eddb70b2ff0167f2f
Mario Benito Rodríguez <mbr@MacBook-Pro-de-Mario.local>
2019-10-19 22:31:20 +0200

    First version of web projectpresentation

--- TubiAPP/src/app/Modules/inicio/inicio.component.ts
+++ TubiAPP/src/app/Modules/inicio/inicio.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  team: any[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
