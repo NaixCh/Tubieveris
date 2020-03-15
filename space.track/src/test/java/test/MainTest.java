50d0dc8fad7f81f25ef72906af3f3020c3a0b329
Christian Escuredo <titianescuredo@gmail.com>
2020-03-15 16:33:38 +0100

    Added test

---
+++ space.track/src/test/java/test/MainTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class MainTest {
+	
+	@Test
+	public void mainTest() {
+		assertTrue(true);
+	}
+}
